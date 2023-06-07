b7192e1cd3370152841cac328c15a8ca502a65d1
Burgiir <89510310+Burgiir@users.noreply.github.com>
2023-06-07 10:23:30 +0000

    render time

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -11,6 +11,16 @@ export const handle: Handle = async ({ event, resolve }) => {
       event.locals.session = result.session;
     }
   }
+  if (event.request.method === "OPTIONS") {
+    return new Response(null, {
+    headers: {
+    "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+    "Access-Control-Allow-Origin": "*",
+    },
+    });
+    }
 
-  return resolve(event);
+    const response = await resolve(event);
+    response.headers.append("Access-Control-Allow-Origin", `*`);
+    return response;
 };
